92edaa24af6e69e409b022fa1ddc6b8055403fe8
lordrichard6 <paulolopesreizinho@gmail.com>
2021-02-20 19:13:53 +0100

    create model user schema

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag'); 
 const mongoose = require('mongoose')
 
-const { MONGODB } = require('config.js')
+const { MONGODB } = require('./config')
 
 const typeDefs = gql`
     type Query {
@@ -23,6 +23,7 @@ const server = new ApolloServer({
 
 mongoose.connect(MONGODB, { useNewUrlParser: true })
     .then(() => {
+        console.log('MongoDB connected')
         return server.listen({ port:5000 })
     })
     .then(res => {
