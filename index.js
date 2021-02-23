5abecea661ef310f631173e331f839fe9a4e07c2
lordrichard6 <paulolopesreizinho@gmail.com>
2021-02-23 17:59:28 +0100

    extract post resolvers

--- index.js
+++ index.js
@@ -6,19 +6,6 @@ const typeDefs = require("./graphql/typeDefs");
 const Post = require("./models/Post.js");
 const { MONGODB } = require("./config");
 
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (err) {
-        throw new Error(err);
-      }
-    },
-  },
-};
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
