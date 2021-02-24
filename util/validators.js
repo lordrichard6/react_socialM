57599cc77267ed7dd9d9e4bf2ba56570f45750d6
lordrichard6 <paulolopesreizinho@gmail.com>
2021-02-24 15:13:55 +0100

    create user login resolver and login mutation

--- util/validators.js
+++ util/validators.js
@@ -16,13 +16,27 @@ module.exports.validateRegisterInput = (
       errors.email = "Email must be a valid email adress";
     }
   }
-  if(password === ''){
-      errors.password = 'Password must not be empty'
-  } else if(password !== confirmPassword) {
-      errors.confirmPassword = 'Password must match'
+  if (password === "") {
+    errors.password = "Password must not be empty";
+  } else if (password !== confirmPassword) {
+    errors.confirmPassword = "Password must match";
   }
   return {
-      errors,
-      valid: Object.keys(errors).length < 1
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+};
+
+module.exports.validateLoginInput = (username, password) => {
+  const errors = {};
+  if (username.trim() === "") {
+    errors.username = "Username must not be empty";
   }
+  if (password.trim() === "") {
+    errors.password = "Password must not be empty";
+  }
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
 };
