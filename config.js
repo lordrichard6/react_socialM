3adea31c8c16a937736505226afc01d0f5eed0bc
lordrichard6 <paulolopesreizinho@gmail.com>
2021-02-24 12:21:37 +0100

    check for user or give error

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGODB: 'mongodb+srv://lordrichard:mongolau31@cluster0.hkjnh.mongodb.net/merng?retryWrites=true&w=majority'
+    MONGODB: 'mongodb+srv://lordrichard:mongolau31@cluster0.hkjnh.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'secret key'
 }
--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,11 +1,13 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
 
+const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
 
 module.exports = {
   Mutation: {
-    register(
+    async register(
       _,
       { registerInput: { username, email, password, confirmPassword } },
       context,
@@ -13,23 +15,41 @@ module.exports = {
     ) {
       // TODO: Validate user data
       // TODO: Make sure user doesnt already exist
-      // TODO: Hash password and create an auth token
+      const user = await User.findOne({ username })
+      if(user){
+          throw new UserInputError('Username is taken', {
+              errors: {
+                  username: 'This username is taken'
+              }
+          })
+      }
+      // Hash password and create an auth token
       password = await bcrypt.hash(password, 12);
 
       const newUser = new User({
-          email,
-          username,
-          password,
-          createdAt: new Date().toISOString()
-      })
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
 
       const res = await newUser.save();
 
-      const token = jwt.sign({
+      const token = jwt.sign(
+        {
           id: res.id,
           email: res.email,
-          username: res.username
-      } )
+          username: res.username,
+        },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      );
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
     },
   },
 };
